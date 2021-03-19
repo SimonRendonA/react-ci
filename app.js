9db87b2cb055f6afa914c581191436eebf9317ab
SimonRendonA <60193839+SimonRendonA@users.noreply.github.com>
2021-03-18 21:36:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(PORT, () => {
-  console.log('Server started on port 5000');
+  console.log("Server started on port 5000");
 });
